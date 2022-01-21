55c9a88d4090ca76fc96fff21bde374e89b0d31a
John Wesley Baik <johnwbaik@gmail.com>
2022-01-20 22:10:53 -0800

    Time: 130 ms (29.31%), Space: 46.9 MB (10.99%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    let lowerCase = s.toLowerCase();
+    let array = lowerCase.split('');
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if ((lowerCase[i] !== '' && lowerCase.charCodeAt(i) >= 97 && lowerCase.charCodeAt(i) <= 122) || (lowerCase.charCodeAt(i) >= 48 && lowerCase.charCodeAt(i) <= 57)) {
+            newArray.push(array[i]);
+        }
+    }
+    let string1 = newArray.join('');
+    let reverseArray = newArray.reverse();
+    let reverseString = reverseArray.join('');
+    console.log('original:', string1);
+    console.log('reverse:', reverseString);
+    if (reverseString === string1) {
+        return true;
+    } else {
+    return false;
+    }
+};
