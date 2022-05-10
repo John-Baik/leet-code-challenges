ad95719a82f7fb486ac43a3e8cab7e94c92264f6
John Wesley Baik <johnwbaik@gmail.com>
2022-05-10 14:00:39 -0700

    Time: 70 ms (78.96%), Space: 42.4 MB (75.40%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    let prefix = '';
+    let maxPrefixLength = Math.min(...strs.map(word => word.length));
+    for (let i = 0; i < maxPrefixLength; i++) {
+        let char = strs[0][i];
+        if (strs.every(current => current[i] === char)) {
+            prefix += char;
+        } else {
+            break;
+        }
+    }
+    return prefix;
+};
+
+
